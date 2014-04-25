531485f928bc032d895c6e085143691fef50a48c
Robin Böhm <robinboehm@googlemail.com>
2014-04-25 11:57:26 +0200

    small improvements to animationChange

---
+++ www/scripts/controllers/animationStyleCtrl.js
@@ -0,0 +1,6 @@
+"use strict";
+
+angular.module('codeQuiz')
+  .controller('animationStyleCtrl', function ($scope) {
+    $scope.animationClass = '';
+  });
--- www/scripts/directives/welcomeAnimation.js
+++ www/scripts/directives/welcomeAnimation.js
@@ -1,19 +1,14 @@
 "use strict";
 
-// TODO: Improve this hacky stuff :)
-// Run special welcome animation only on first screen
-// Best way to do this here?
 angular.module('codeQuiz')
-  .directive('welcomeAnimation', function ($animate, $timeout) {
+  .directive('welcomeAnimation', function () {
     return {
-      link: function (scope, elem) {
-        $animate.addClass(elem.parent(), 'welcomeAnimation');
+      link: function (scope) {
+        var animationClass = scope.animationClass;
+        scope.animationClass = 'welcomeAnimation'
 
         scope.$on('$destroy', function () {
-          $timeout(function () {
-            $animate.removeClass(elem.parent(), 'welcomeAnimation');
-          },5000);
-
+          scope.animationClass = animationClass;
         });
       }
     }
