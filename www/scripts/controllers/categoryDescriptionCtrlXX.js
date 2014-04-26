3324c3d9f5a3871d1c7d40c15eabdd0ae92f4229
Elmar Burke <hallo@elmarburke.de>
2014-04-26 14:55:58 +0200

    no message

--- www/scripts/controllers/categoryDescriptionCtrlXX.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-
-angular.module('codeQuiz')
-  .controller('CategoryDescriptionCtrl', function ($scope, $routeParams, $sce, DataHolderService, GameInstanceService) {
-    $scope.category = $routeParams.category;
-    GameInstanceService.createGameFor($routeParams.category);
-    $scope.description = $sce.trustAsHtml(DataHolderService.getDescriptionFor($routeParams.category));
-  });
